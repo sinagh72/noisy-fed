82277759da866ca66c48fdcdec391fc4cd91bdb3
sina <sina.gholami72@gmail.com>
2026-02-11 17:56:01 -0500

    fix CIFAR10 image transformation

--- fine/dataset/dataset.py
+++ fine/dataset/dataset.py
@@ -22,7 +22,7 @@ def build_transforms(dataset_name: str, split: str):
     if dataset_name in ["cifar10", "cifar100"]:
         if split == "train":
             return transforms.Compose([
-                transforms.Resize((32, 32), interpolation=transforms.InterpolationMode.BICUBIC),
+                transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize(
