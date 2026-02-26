e82080cacb30595ee74665ad6459ad2328356e58
sina <sina.gholami72@gmail.com>
2026-02-26 14:01:46 -0500

    Add cutmix and relabeling configs

--- fine/configs.py
+++ fine/configs.py
@@ -7,14 +7,14 @@ class ExpConfig:
     dataset_name: str = "cifar10"
 
     iid: bool = True
-    non_iid_prob_class: float = 0.0
-    alpha_dirichlet: float = 0.0
+    non_iid_prob_class: float = 1.0
+    alpha_dirichlet: float = 0.1
     num_users: int = 10
 
     # noise
-    level_n_system: float = 0.5
-    noise_p: float = 0.7
-    noise_type: str = "asym"
+    level_n_system: float = 0.9
+    noise_p: float = 0.3
+    noise_type: str = "sym"
     save_dir: str = "./results"
 
     # model
@@ -22,18 +22,20 @@ class ExpConfig:
     pretrained: bool = True
     batch_size: int = 64
     
+    # cutmix
+    use_cutmix: bool = True
+    cutmix_alpha: float = 1.0   # 0.5–1.0 common
+    cutmix_p: float = 0.5       # 0.3–0.7 common
+
     # FL
     fl_strategy: str = "fedeig"
     rounds: int = 100
 
     # relabel
     identification_epochs: int = 5
-    tau: float = 0.95
+    cleaning_rounds = 10
 
-
-    strict_keep_frac: float = 0.30
-    strict_min_per_class: int = 10
-
+    # training param
     epochs: int = 1
     lr: float = 5e-5
     weight_decay: float = 1e-2
